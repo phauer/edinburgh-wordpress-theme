4ce80bb48609190fa0e95c7c54e470caa8eebda2
phauer <info@philipphauer.de>
2016-09-06 16:11:03 +0200

    support featured image in backend. fix to big thumbnails.

--- theme/functions.php
+++ theme/functions.php
@@ -1,4 +1,19 @@
 <?php
+if (function_exists( 'add_theme_support')) {
+  //Add default posts and comments RSS feed links to head
+  add_theme_support('automatic-feed-links');
+  //Enable support for Post Thumbnails on posts and pages
+  //@link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+  add_theme_support('post-thumbnails');
+  add_image_size( 'mythumbnails', 150, 9999 ); //300 pixels wide (and unlimited height)
+  //Enable support for Post Formats
+  add_theme_support('post-formats', array('aside', 'image', 'video', 'quote', 'link'));
+  //Setup the WordPress core custom background feature.
+  add_theme_support('custom-background', apply_filters('jokkmokk_custom_background_args', array(
+    'default-color' => 'ffffff',
+    'default-image' => '',
+  )));
+}
 remove_filter ('the_content', 'wpautop'); //remove <p> around "Continue Reading..."
 
 function eb_widgets_init() {
