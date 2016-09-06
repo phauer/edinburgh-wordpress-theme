7fb1e5663fe7e4fafcb843462d7481bb56c05fb3
phauer <info@philipphauer.de>
2016-09-06 18:23:30 +0200

    add categories and tag at the bottom of a post

--- theme/index.php
+++ theme/index.php
@@ -14,7 +14,31 @@
       <?php endif; ?>
       <p><?php the_content(''); ?></p>
     </section>
-    <footer></footer>
+    <footer class="eb-entry-meta">
+      <?php if (is_single()) :?>
+      <p><?php
+        $categories_list = get_the_category_list( __( ', ', 'edingburgh' ) );
+        $tag_list = get_the_tag_list( '', __( ', ', 'edingburgh' ) );
+        if ( '' != $tag_list ) {
+          $utility_text = __( 'This entry was posted in %1$s and tagged %2$s.', 'edingburgh' );
+        } elseif ( '' != $categories_list ) {
+          $utility_text = __( 'This entry was posted in %1$s.', 'edingburgh' );
+        } else {
+          $utility_text = __( 'This entry was posted.', 'edingburgh' );
+        }
+        printf(
+          $utility_text,
+          $categories_list,
+          $tag_list,
+          esc_url( get_permalink() ),
+          the_title_attribute( 'echo=0' ),
+          get_the_author(),
+          esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
+        );
+        ?>
+      </p>
+      <?php endif; ?>
+    </footer>
   </article>
   <?php endwhile; else: ?>
   <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
